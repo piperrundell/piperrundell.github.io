7a901202439f924395b73eab47b951811830d095
piperrundell <piru4472@colorado.edu>
2022-06-21 17:44:43 -0600

    push1

--- exercise9:InteractiveElements/scripts.js
+++ exercise9:InteractiveElements/scripts.js
@@ -4,10 +4,12 @@ para.addEventListener('click', updateName);
 
 function updateName(){
   const name = prompt('Enter a New Name');
-  para.textContent = `Player Name: ${name}`;
+  para.textContent = `Player 1: ${name}`;
+
 }
 
 document.getElementById("mydiv").onclick = function(){
-  this.style.backgroundColor= "pink";
+  this.style.backgroundColor= "blue";
   this.style.color="white";
+  
 }
