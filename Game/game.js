64078db194dbb1d5f0cead3ec4e6477eec229151
piperrundell <piru4472@colorado.edu>
2022-06-29 11:10:05 -0600

    6/29/22

--- Game/game.js
+++ Game/game.js
@@ -1,14 +1,13 @@
-//Shuffle API endpoint
-const endpoint = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
-
-console.log(endpoint);
+//Shuffle Stuff //
+const shuffle = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+console.log(shuffle);
 
 const shuffleButton = document.querySelector('#shuffle-deck');
 shuffleButton.addEventListener('click', getShuffle);
 
 async function getShuffle(){
   try{
-    const response = await fetch(endpoint);
+    const response = await fetch(shuffle);
     if(!response.ok){
       throw Error(response.statusText);
     }
@@ -21,6 +20,32 @@ async function getShuffle(){
   }
   function displayShuffle(shuffle){
     const shuffleText = document.querySelector('#js-quote-text');
-    shuffleText.textContent = "shuffled";
+    shuffleText.textContent = "✓";
+  }
+}
+
+// Draw Card Stuff //
+const drawCard = "https://deckofcardsapi.com/api/deck/new/draw/?count=1"
+console.log(drawCard);
+
+const drawButton = document.querySelector('#draw-card');
+drawButton.addEventListener('click', getCard);
+
+async function getCard(){
+  try{
+    const response = await fetch (drawCard);
+    if(!response.ok){
+      throw Error(response.statusText);
+    }
+    const json = await response.json()
+    displayCard(json);
+  }
+    catch (err) {
+      console.log(err);
+      alert('failed');
+    }
+  function displayCard(drawCard){
+    const cardReturn = document.querySelector('#js-quote-text');
+    cardReturn.textContent = "hi";
   }
 }
